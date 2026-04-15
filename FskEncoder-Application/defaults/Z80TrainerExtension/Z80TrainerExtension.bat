ffd531be259e081a53b331bd5c186cabf7bd45aa
kamaSo-Macha <Stefan.Dickel@t-online.de>
2026-04-15 12:28:16 +0200

    Cosmetics and fixes

--- FskEncoder-Application/defaults/Z80TrainerExtension/Z80TrainerExtension.bat
+++ FskEncoder-Application/defaults/Z80TrainerExtension/Z80TrainerExtension.bat
@@ -2,4 +2,4 @@ rem
 rem append this line to the CLASSPATH definition in 
 rem the extension section of the FskEncoder.bat file
 
-set CLASS_PATH=%CLASS_PATH%;../extensions/Z80TrainerMonitorExtension.jar;
+set CLASS_PATH=%CLASS_PATH%;../extensions/Z80TrainerExtension.jar;
